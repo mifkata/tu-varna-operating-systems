bb6d63f2f746dc03e1e99b0216d7707c604509d3
Andriyan Ivanov <andriyan.ivanov@gmail.com>
2026-03-25 01:03:28 +0200

    feat: added app5

---
+++ app5/common.h
@@ -0,0 +1,8 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include "../shared/shared.h"
+
+#define SEED_ID 23623452
+
+#endif
---
+++ app5/producer.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "common.h"
+
+int main(void)
+{
+    char *buf = (char *)getmem(SEED_ID);
+    sem_t sem_empty = sem_init(0);
+    sem_t sem_full = sem_init(1);
+    sem_set(sem_empty, 1);
+    sem_set(sem_full, 0);
+
+    for (char c = 'a'; c <= 'z'; c++)
+    {
+        PS(sem_empty);
+        buf[0] = c;
+        printf("Произведено: %c\n", c);
+        VS(sem_full);
+    }
+
+    sem_remove(sem_empty);
+    sem_remove(sem_full);
+    return 0;
+}
